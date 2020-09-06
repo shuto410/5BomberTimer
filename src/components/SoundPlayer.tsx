577911d0757053944ebfbe1ff85cf07cfe5dba8d
shuto410 <shuto@posl.ait.kyushu-u.ac.jp>
2020-09-07 05:28:35 +0900

    SE再生機能

--- src/components/SoundPlayer.tsx
+++ src/components/SoundPlayer.tsx
@@ -5,14 +5,38 @@ import RemoveIcon from '@material-ui/icons/Remove';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import StopIcon from '@material-ui/icons/Stop';
 import PauseIcon from '@material-ui/icons/Pause';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import ClearIcon from '@material-ui/icons/Clear';
 import 'fontsource-roboto'
 import Time from './Time';
 
 const SoundPlayer: React.FC = () => {
-  const [audio] = React.useState(new Audio("../../audio/correct.mp3"));
+  const [correctSound] = React.useState(new Audio("https://raw.githubusercontent.com/shuto410/5BomberTimer/master/audio/correct.mp3"));
+  const [wrongSound] = React.useState(new Audio("https://raw.githubusercontent.com/shuto410/5BomberTimer/master/audio/wrong.mp3"));
+  useEffect(() => {
+    document.body.addEventListener('keydown',
+      (event) => {
+        if (event.key === 'c') {
+          correctSound.play();
+        }
+      }
+    );
+    document.body.addEventListener('keydown',
+      (event) => {
+        if (event.key === 'x') {
+          wrongSound.play();
+        }
+      }
+    );
+  }, []);
   return (
     <div>
-      <Button onClick={() => audio.play()}>Correct</Button>
+      <Box mt={3}>
+        <Grid container justify="center">
+          <IconButton onClick={() => correctSound.play()}><RadioButtonUncheckedIcon></RadioButtonUncheckedIcon></IconButton>
+          <IconButton onClick={() => wrongSound.play()}><ClearIcon></ClearIcon></IconButton>
+        </Grid>
+      </Box>
     </div>
   );
 
